d8c3ca823a62ad3d690d5323de41227809341624
Jeacson_Snake <Jeacson_Snake@outlook.com>
2023-08-16 12:16:56 +0800

    feat: 尝试针对多语言进行支持
    尝试在 .vuepress\config.js 中添加locales配置项以设置生成的html中的lang属性

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -27,6 +27,20 @@ module.exports = {
                 text: 'Github',
                 link: 'https://Github.com'
             },
-        ]
+        ],
+        locales: {
+            // 键名是该语言所属的子路径
+            // 作为特例，默认语言可以使用 '/' 作为其路径。
+            '/': {
+                lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+                title: 'VuePress',
+                description: 'Vue 驱动的静态网站生成器'
+            },
+            '/en/': {
+                lang: 'en-US',
+                title: 'VuePress',
+                description: 'Vue-powered Static Site Generator'
+            }
+        }
     }
 }
