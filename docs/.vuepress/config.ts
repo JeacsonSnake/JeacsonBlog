24230fc166f7908a247fef72a2f016561d4331de
Jeacson_Snake <Jeacson_Snake@outlook.com>
2023-08-19 19:03:12 +0800

    refactor: 将config文件替换为TypeScript格式，并对其进行小部分拆分

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,22 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme";
+
+export default defineUserConfig({
+
+    theme,
+
+    locales: {
+        // 键名是该语言所属的子路径
+        // 作为特例，默认语言可以使用 '/' 作为其路径。
+        '/': {
+            lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
+            title: 'JeacsonBlog-一个简单的博客',
+            description: '使用VuePress搭建的个人博客网站'
+        },
+        '/en/': {
+            lang: 'en-US',
+            title: 'JeacsonBlog-A Simple Blog',
+            description: 'A personal blog website powered by Vuepress'
+        }
+    },
+});
---
+++ docs/.vuepress/theme.ts
@@ -0,0 +1,71 @@
+import { hopeTheme } from "vuepress-theme-hope";
+
+/* Your themeConfig here */
+
+export default hopeTheme({
+    // 导航栏相关设置
+    logo: '/assets/img/logo.png',
+    navbar: [{
+            text: '主页',
+            link: '/'
+        },
+        {
+            text: '学习笔记',
+            link: '/learningnote/'
+        },
+        {
+            text: '关于',
+            link: '/about/'
+        },
+        {
+            text: '语言',
+            ariaLabel: 'Language Menu',
+            children: [{
+                    text: '中文',
+                    link: '/'
+                },
+                {
+                    text: 'English',
+                    link: '/en/'
+                }
+            ]
+        },
+        {
+            text: 'Github',
+            link: 'https://Github.com'
+        },
+    ],
+    sidebar: {
+        "/learningNote/": "structure"
+    },
+
+    // sidebar: [{
+    //     text: '学习笔记',
+    //     link: '/learningnote/',
+    //     collapsable: false,
+    //     // sidebarDepth: 1,
+    //     children: [{
+    //             text: 'VUE3',
+    //             prefix: '/learningnote/VUE3/',
+    //             collapsible: false,
+    //             sidebarDepth: 2,
+    //             children: [
+    //                 'VUE3_Vs_VUE2_In_Reactive_Data',
+    //                 'VUE3_Vs_VUE2_In_Watch',
+    //                 'VUE3_Vs_VUE2_In_Render_Function'
+    //             ]
+    //         },
+    //         {
+    //             text: 'JavaScript',
+    //             prefix: '/learningnote/JSNote/',
+    //             collapsable: false,
+    //             sidebarDepth: 2,
+    //             children: [
+    //                 'Formal&Real_Para_When_Func_Get_Obj',
+    //             ],
+    //         }
+    //     ],
+    // }],
+
+
+})
