979c84d95379c8dabf7db537a79f083388d58da6
Jeacson_Snake <Jeacson_Snake@outlook.com>
2023-09-18 10:07:04 +0800

    build: 将用户设置的base进行注释以尝试适配自定义域名

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,7 +4,7 @@ import theme from "./theme";
 export default defineUserConfig({
     title: "JeacsonBlog",
     description: "JeacsonBlog-一个简单的博客",
-    base:'/JeacsonBlog/',
+    // base:'/JeacsonBlog/',
 
     theme,
 
